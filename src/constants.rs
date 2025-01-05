24c0d7ed494cfd57b39c2b1e95c61ef88dd2ad5a
Dana Ross <dana@danaross.dev>
2025-01-05 09:50:30 -0500

    Initial task of arg parsing with clap

---
+++ src/constants.rs
@@ -0,0 +1,9 @@
+pub const APP_NAME: &str = "lls";
+
+pub const RELEASE_VERSION: &str = "1.0";
+
+pub const SHORT_DESCRIPTION: &str = "💖 Dana's fun little ls alternative";
+
+pub const LONG_DESCRIPTION: &str = "I don't know what to say. I was doing the Rust-by-Example tutorial. Next thing I knew it, I was writing my own ls which totally seems like the kind of thing Rust developers are prone to doing.\n
+\n
+Nothing helps you learn a language better than trying to deal with the filesystem in it.";
