459fead9d23f6ca8e10365a3b82fb55dd73fa82d
Dana Ross <dana@danaross.dev>
2025-01-05 16:28:56 -0500

    Started implementing command line arguments - -l for long listings (like ls) - PATH to display

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ mod constants;
 use std::env;
 use std::process;
 use std::fs;
+use std::path::PathBuf;
+use std::io::Error;
 use icons::get_icons_for_direntry;
 use massage_direntries::marshall_directory_entries;
 use clap::Parser;
@@ -15,7 +17,11 @@ use clap::Parser;
 #[command(version = constants::RELEASE_VERSION)]
 #[command(about = constants::SHORT_DESCRIPTION, long_about = constants::LONG_DESCRIPTION)]
 struct Args {
+    path: Option<String>,
 
+    /// use a long listing format
+    #[arg(short, long, default_value_t = false)]
+    long: bool,
 }
 
 fn main() {
@@ -25,27 +31,47 @@ fn main() {
 fn lls() -> i32 {
     let args = Args::parse();
 
-    let current_dir = match env::current_dir() {
+    let path = match get_path(&args) {
         Ok(path) => path,
-        Err(_) => { println!("Problem getting the current directory");return 1;}
+        Err(_) => { println!("Problem getting the target directory");return 1;}
     };
 
-    let read_dir_result = fs::read_dir(current_dir);
+    let read_dir_result = fs::read_dir(path);
     let contents = match read_dir_result {
         Ok(contents) => contents,
         Err(_) => {println!("Problem reading directory contents"); return 2;}
     };
 
     let directory_entries = marshall_directory_entries(contents);
-    
     for entry in directory_entries {
         let permission_effects = permissions::permission_effects_for_direntry(&entry);
         let file_icons = get_icons_for_direntry(&entry, permission_effects);
-        
-        print!("[{}{}{}] ", file_icons, if !file_icons.is_empty() { " " } else { "" }, entry.file_name().to_str().unwrap());
-    }
 
-    print!("\n");
+        if args.long {
+            long_listing(&entry, &file_icons);
+        }
+        else {
+            short_listing(&entry, &file_icons);
+        }
+    }
     
     return 0;
+}
+
+fn short_listing(entry: &fs::DirEntry, file_icons: &str) -> () {
+    print!("[{}{}{}] ", file_icons, if !file_icons.is_empty() { " " } else { "" }, entry.file_name().to_str().unwrap());
+}
+
+fn long_listing(entry: &fs::DirEntry, file_icons: &str) -> () {
+    println!("[{}{}{}] ", file_icons, if !file_icons.is_empty() { " " } else { "" }, entry.file_name().to_str().unwrap());
+}
+
+fn get_path(args: &Args) -> Result<PathBuf, Error> {
+    let requested_path = &args.path;
+
+    if requested_path.is_some() {
+        return Ok(PathBuf::from(requested_path.as_ref().unwrap()));
+    } else {
+        return env::current_dir();
+    }
 }
